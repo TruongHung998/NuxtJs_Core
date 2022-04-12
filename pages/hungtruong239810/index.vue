fce6ea82fd1b4e9d26f7cbc5b455ecfe5f348ea1
HungEplus <truonghung239810@gmail.com>
2022-04-12 15:59:08 +0700

    _: done animation

--- pages/hungtruong239810/index.vue
+++ pages/hungtruong239810/index.vue
@@ -12,15 +12,79 @@
         .content-container
           img(:src="'/content2.png'").content-2
         .content-container
-          img(:src="'/content1.png'").content-1
+          img(:src="'/content3.png'").content-3
+    .body-content
+      .content
 </template>
 
 <script>
-import { gsap } from 'gsap'
+import {gsap} from 'gsap'
+
 export default {
+  data() {
+    return {
+      windowTop: 0
+    }
+  },
   mounted() {
-    console.log('isRun')
-    // gsap.set(".sharp-1", { transform: 'rotateY(35deg) rotateX(10deg)' });
+    gsap.to(".sharp-1", {
+      transform: 'rotateY(35deg) rotateX(10deg)',
+    });
+    window.addEventListener("scroll", this.onScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll)
+  },
+  watch: {
+    windowTop: {
+      immediate: true,
+      handler(val) {
+        console.log(val)
+        console.log(val)
+        switch (true) {
+          case val < 710:
+            gsap.to(".sharp-1", {
+              transform: 'rotateY(35deg) rotateX(10deg)',
+            });
+            gsap.set('.sharp-1', {
+              attr: {src: '/sharp1.png'},
+              ease: 'none',
+              duration: 0.3,
+            });
+            break
+          case val < 1550:
+            gsap.to(".sharp-1", {
+              duration: 0.3,
+              ease: 'none',
+              x: 400,
+              transform: 'rotate(-6.0001deg) rotateY(-39.6407deg) rotateX(27.2366deg)',
+            });
+            gsap.set('.sharp-1', {
+              attr: {src: '/sharp2.png'},
+              ease: 'none',
+              duration: 0.3,
+            });
+            break
+          case val >= 1550:
+            gsap.to(".sharp-1", {
+              duration: 0.3,
+              ease: 'none',
+              transform: 'rotateY(35deg) rotateX(10deg)',
+            });
+            gsap.set('.sharp-1', {
+              attr: {src: '/sharp3.png'},
+              ease: 'none',
+              duration: 0.3,
+            });
+            break
+        }
+      }
+    }
+  },
+  methods: {
+    onScroll(e) {
+      this.windowTop = window.top.scrollY /* or: e.target.documentElement.scrollTop */
+    }
   }
 }
 </script>
